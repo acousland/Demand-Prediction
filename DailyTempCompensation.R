1815ed45a1aa16bfe3e5b88bb31d6ee175597467
Aaron Cousland <acousland@gmail.com>
2015-06-11 14:27:38 +1000

    neural network added

--- DailyTempCompensation.R
+++ DailyTempCompensation.R
@@ -55,6 +55,12 @@ colnames(power.log) <- c("Date","NMI","Demand","Max_Temp")
 
 # Calculate the day of the week
 power.log$Day <- weekdays(power.log$Date)
+power.log$DayNumber <- ifelse(power.log$Day == 'Monday', 1,
+                              ifelse(power.log$Day == 'Tuesday',2,
+                                     ifelse(power.log$Day == 'Wednesday',3,
+                                            ifelse(power.log$Day == 'Thursday',4,
+                                                   ifelse(power.log$Day == 'Friday',5,
+                                                          ifelse(power.log$Day == 'Saturday',6,7))))))
 
 
 # Set up graphing parameters and plot graphs
@@ -71,15 +77,39 @@ par(mfrow=c(1,1))
 
 
 # Train the neural network
-demand.network <- neuralnet(Demand~Max_Temp, data = power.log, hidden=20, threshold=0.1)
+demand.network <- neuralnet(Demand~Max_Temp+DayNumber, data = power.log, hidden=20, threshold=1,rep=2)
 
-#plot(demand.network)
+#plot.nn(demand.network)
 
 temp.compensation <- prediction(demand.network)
-plot(temp.compensation$rep1)
-
-#plot(temp.compensation$data, type = "l")
 
 temp.range <- 10:50
-predicted.demand <- compute(demand.network,temp.range)
-plot(temp.range,predicted.demand$net.result,type = "l", ylim = c(0,max(predicted.demand$net.result)))
+day.range <- 1:7
+
+
+
+ymax=4
+par(mfrow=c(2,4))
+
+predicted.demand <- compute(demand.network,data.frame(temp.range,1))
+plot(temp.range,predicted.demand$net.result,type = "l", ylim = c(0,ymax), main = "Monday")
+
+predicted.demand <- compute(demand.network,data.frame(temp.range,2))
+plot(temp.range,predicted.demand$net.result,type = "l", ylim = c(0,ymax), main = "Tuesday")
+
+predicted.demand <- compute(demand.network,data.frame(temp.range,3))
+plot(temp.range,predicted.demand$net.result,type = "l", ylim = c(0,ymax), main = "Wednesday")
+
+predicted.demand <- compute(demand.network,data.frame(temp.range,4))
+plot(temp.range,predicted.demand$net.result,type = "l", ylim = c(0,ymax), main = "Thursday")
+
+predicted.demand <- compute(demand.network,data.frame(temp.range,5))
+plot(temp.range,predicted.demand$net.result,type = "l", ylim = c(0,ymax), main = "Friday")
+
+predicted.demand <- compute(demand.network,data.frame(temp.range,6))
+plot(temp.range,predicted.demand$net.result,type = "l", ylim = c(0,ymax), main = "Saturday")
+
+predicted.demand <- compute(demand.network,data.frame(temp.range,7))
+plot(temp.range,predicted.demand$net.result,type = "l", ylim = c(0,ymax), main = "Sunday")
+
+
